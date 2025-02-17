13744b857d701443293c198355b8ef8faf7f2135
YashPathare <yashpathare51@gmail.com>
2025-02-17 22:14:51 +0530

    58 5:11

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import {BrowserRouter as Router,Route,Routes,} from "react-router-dom";
 import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 function App() {
   return (
     <>
-      <Navbar/>
-      <h1>This is iNotebook</h1>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route exact path='/about' element={<About/>}/>
+        </Routes>
+      </Router>
     </>
   );
 }
