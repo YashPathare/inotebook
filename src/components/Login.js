c31b066d6739023acd624ccbc4cdccaf6b251144
YashPathare <yashpathare51@gmail.com>
2025-03-04 19:31:48 +0530

    70 2:15

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,9 @@
 import React,{useState} from 'react'
+import {useNavigate} from 'react-router-dom'
 
 function Login() {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-
+    const [credentials, setCredentials] = useState({email:"",password:""})
+    let navigate = useNavigate();
     const handleSubmit= async (e)=>{
         e.preventDefault();
         const response = await fetch("http://localhost:5000/api/auth/login", {
@@ -11,14 +11,23 @@ function Login() {
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({email,password}),
+            body: JSON.stringify({email:credentials.email,password:credentials.password}),
         });
           const json =await response.json()
           console.log(json);
+          if (json.success){
+            //Save the auth token and redirect  
+            localStorage.setItem('token',json.authtoken);
+            navigate("/");
+          }
+          else
+          {
+            alert("Invalid Credentials")
+          }
     }
 
     const onChange =(e) =>{
-        setNote({...note,[e.target.name]:e.target.value}) 
+        setCredentials({...credentials,[e.target.name]:e.target.value}) 
     }
 
   return (
@@ -28,7 +37,7 @@ function Login() {
                 <label htmlFor="email" className="form-label">
                     Email address
                 </label>
-                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" name="email"/>
+                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" name="email" value={credentials.email} onChange={onChange}/>
                 <div id="emailHelp" className="form-text">
                     We'll never share your email with anyone else.
                 </div>
@@ -37,7 +46,7 @@ function Login() {
                 <label htmlFor="password" className="form-label">
                     Password
                 </label>
-                <input type="password" className="form-control" id="password" name="password"/>
+                <input type="password" className="form-control" id="password" name="password" value={credentials.password} onChange={onChange}/>
             </div>
             <button type="submit" className="btn btn-danger">
                 Submit
